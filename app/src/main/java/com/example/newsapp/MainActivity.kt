eef44425e2fcaab87d885896cb799d47ee420bed
JisooJudy <cjslove0530@gmail.com>
2023-07-14 20:53:12 +0900

    [7/14] og:image 가져오기

--- app/src/main/java/com/example/newsapp/MainActivity.kt
+++ app/src/main/java/com/example/newsapp/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.newsapp.databinding.ActivityMainBinding
 import com.tickaroo.tikxml.TikXml
 import com.tickaroo.tikxml.retrofit.TikXmlConverterFactory
+import org.jsoup.Jsoup
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -45,7 +46,34 @@ class MainActivity : AppCompatActivity() {
             override fun onResponse(call: Call<NewsRss>, response: Response<NewsRss>) {
                 Log.e("MainActivity","${response.body()?.channel?.items}")
 
-                newsAdapter.submitList(response.body()?.channel?.items.orEmpty())
+                val list = response.body()?.channel?.items.orEmpty().transform()
+                newsAdapter.submitList(list)
+
+                list.forEachIndexed { index,news ->
+                    //og:image meta에서 가져오기
+                    Thread {
+                        try{
+                            val jsoup = Jsoup.connect(news.link).get()
+                            val elements = jsoup.select("meta[property^=og:]")
+                            val ogImageNode = elements.find { node ->
+                                node.attr("property") == "og:image"
+                            }
+
+                            news.imageUrl = ogImageNode?.attr("content")
+                            Log.e("MainActivity","imageUrl: ${news.imageUrl}")
+
+                        }catch (e: Exception){
+                            e.printStackTrace()
+                        }
+
+                        runOnUiThread {
+                            newsAdapter.notifyItemChanged(index)
+                        }
+                    }.start()
+
+                }
+
+
             }
 
             override fun onFailure(call: Call<NewsRss>, t: Throwable) {
